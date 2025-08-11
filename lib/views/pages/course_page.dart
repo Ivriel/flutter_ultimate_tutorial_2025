2911cc61421fbd9b584b0b101cb39c22ab5e08c9
Ivriel Gunawan <gunawanivriel@gmail.com>
2025-08-11 16:17:09 +0700

    Get data from api

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,19 +1,42 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_ultimate_tutorial_2025/data/constants.dart';
-import 'package:flutter_ultimate_tutorial_2025/views/widgets/hero_widget.dart';
+import 'dart:developer';
 
-class CoursePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_ultimate_tutorial_2025/views/widgets/hero_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    List<String> list = [
-      KValue.keyConcepts,
-      KValue.cleanUi,
-      KValue.fixBugs,
-      KValue.basicLayout,
-    ];
+  State<CoursePage> createState() => _CoursePageState();
+}
 
+class _CoursePageState extends State<CoursePage> {
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  void getData() async {
+    var url = Uri.https('bored-api.appbrewery.com','/random');
+    var response = await http.get(url);
+
+    if(response.statusCode == 200) {
+      var jsonResponse = jsonDecode(response.body) as Map<String,dynamic>; // convert dulu ke map pakai json decode biar bisa dibaca 
+      var itemCount = jsonResponse['activity'];
+      print('One of simple activity: $itemCount');
+      log(response.body);
+    } else {
+      print("Request failed with status: ${response.statusCode}.");
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),// supaya ada back buttonnya
       body: const  Padding(
@@ -23,7 +46,7 @@ class CoursePage extends StatelessWidget {
         child: Column(
           children: [
            HeroWidget(
-              title: 'Flutter Mapp'
+              title: 'Course'
             )
           ],
         ),
