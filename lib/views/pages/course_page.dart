e30b40330089a8bd77829cacd4ff0c1f79bd77df
Ivriel Gunawan <gunawanivriel@gmail.com>
2025-08-11 17:03:46 +0700

    feat: Create Activity class and update CoursePage to fetch and display activity data
    - Added Activity class to model activity data with JSON parsing.
    - Refactored getData method in CoursePage to return an Activity object.
    - Implemented FutureBuilder in CoursePage to handle asynchronous data loading and display activity details.

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_ultimate_tutorial_2025/data/classes/activity_class.dart';
 import 'package:flutter_ultimate_tutorial_2025/views/widgets/hero_widget.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -13,45 +14,70 @@ class CoursePage extends StatefulWidget {
 }
 
 class _CoursePageState extends State<CoursePage> {
+  late Future <Activity> activity; // valuenya bakal diset nanti jadi kalau null sekarang gapapa ( itu fungsinya deklarasi late)
 
   @override
   void initState() {
-    getData();
     super.initState();
+    activity = getData();
   }
 
-  void getData() async {
-    var url = Uri.https('bored-api.appbrewery.com','/random');
-    var response = await http.get(url);
+  Future<Activity> getData() async {
+   var url = Uri.https('bored-api.appbrewery.com', '/random');
+  var response = await http.get(url);
 
-    if(response.statusCode == 200) {
-      var jsonResponse = jsonDecode(response.body) as Map<String,dynamic>; // convert dulu ke map pakai json decode biar bisa dibaca 
-      var itemCount = jsonResponse['activity'];
-      print('One of simple activity: $itemCount');
-      log(response.body);
-    } else {
-      print("Request failed with status: ${response.statusCode}.");
+  if (response.statusCode == 200) {
+    try {
+      return Activity.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } catch (e, stack) {
+      log('Parsing error: $e');
+      log('Stack: $stack');
+      rethrow;
     }
-
+  } else {
+    log('Failed to load activity ${response.statusCode}');
+    throw Exception('Failed to load activity from api ${response.statusCode}');
+  }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),// supaya ada back buttonnya
-      body: const  Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-           HeroWidget(
-              title: 'Course'
-            )
-          ],
-        ),
-      ),
-    )
-    );
+        appBar: AppBar(), // supaya ada back buttonnya
+        body: FutureBuilder(
+          future: activity,
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child:  CircularProgressIndicator()
+              );
+            }
+
+            if (snapshot.hasError) {
+              return Center(child: Text("Error: ${snapshot.error}"));
+            }
+
+            if (snapshot.hasData) {
+              Activity  activity =  snapshot.data!;
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: [
+                      HeroWidget(title: activity.activity),
+                      Text(activity.activity)
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return const Center(
+                child: Text("Error"),
+              );
+            }
+          },
+        ));
+
   }
 }
